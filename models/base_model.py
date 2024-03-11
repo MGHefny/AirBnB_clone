cb6018e5dcfc22e6d6df5c7329d391c2a83054cf
MGHefny <mgenedhefny@gmail.com>
2024-03-11 03:32:23 +0200

    bb

--- models/base_model.py
+++ models/base_model.py
@@ -1,14 +1,15 @@
 #!/usr/bin/python3
-
+"""BaseModel class"""
+import models
 import uuid
 from datetime import datetime
-from models import storage
 
 
 class BaseModel:
+    """the method of Base Model class"""
 
     def __init__(self, *args, **kwargs):
-
+        """initializes Base Class"""
         if kwargs is not None and kwargs != {}:
             for key in kwargs:
                 if key == "created_at":
@@ -28,19 +29,18 @@ class BaseModel:
             storage.new(self)
 
     def __str__(self):
-
+        """overriding"""
         return "[{}] ({}) {}".format(type(self).__name__, self.id, self.__dict__)
 
     def save(self):
-
+        """updated_at attribute to the now datetime"""
         self.updated_at = datetime.now()
         storage.save()
 
     def to_dict(self):
-
+        """returns a dictionary representation __dict__ of the instance"""
         in_dict = self.__dict__.copy()
         in_dict["__class__"] = type(self).__name__
         in_dict["created_at"] = in_dict["created_at"].isoformat()
         in_dict["updated_at"] = in_dict["updated_at"].isoformat()
-
-    return in_dict
+        return in_dict
